fa583321427b1a6fe2ec919fc13996142f081057
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2019-12-02 22:04:45 +0300

    Supprimer compte fini

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow () {
 		}
 	})
 	  
-  	mainWindow.loadURL('http://localhost:1903/ajoutcompte.html');
+  	mainWindow.loadURL('http://localhost:1903/supcompte.html');
 
   	mainWindow.on('closed', function () {
 		mainWindow = null
