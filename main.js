114882e1a0828092b8738ecec7e2d7fcc5b332aa
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2019-12-02 12:22:46 +0300

    change cotisation form on web, not yet for electron due electron cache

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow () {
 		}
 	})
 	  
-  	mainWindow.loadURL('http://localhost:1903/login.html');
+  	mainWindow.loadURL('http://localhost:1903/budget.html');
 
   	mainWindow.on('closed', function () {
 		mainWindow = null
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from datetime import datetime
 user, users = '', None
 
 if sys.platform == 'linux':
-	path = '/usr/bin/electron'
+	path = '/usr/bin/electron4'
 
 elif sys.platform == 'win32':
 	path = r'node_modules\electron\dist\electron'
