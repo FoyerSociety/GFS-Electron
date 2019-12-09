596b4a04f960876ae5d5f2034b617247fd5446b3
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-12-09 07:16:35 +0300

    Release 2.0.0b1 Update config

--- main.js
+++ main.js
@@ -52,37 +52,6 @@ app.on('activate', function () {
 if (mainWindow === null) createWindow()
 })
 
- const menu = Menu.buildFromTemplate(
-	 	[
-// 		{
-// 			label: "",
-// 			submenu: [
-// 				{
-// 					label: "Fermer la fenêtre",
-// 					click: function(){
-// 						app.quit()
-// 					}
-// 				}
-// 			]
-// 		},
-// 		{
-// 			label: " ",
-// 			submenu : [
-// 				{
-// 					label: "Mode sombre",
-// 					click: function(){
-// 						mainWindow.backgroundColor = 'black';
-// 					}
-// 				}
-// 			]
-// 		},
-// 		{
-// 			label: ""
-// 		},
-// 		{
-// 			label: ""
-// 		}
-	]
-);
+ 
 
-Menu.setApplicationMenu(menu);
+//Menu.setApplicationMenu(menu);
